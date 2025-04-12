4f3c9549e842049b4b819771b1bb1c38021db578
oacmut <balazs.borbely@stud.uni-corvinus.hu>
2025-04-12 12:59:40 +0200

    updated gitignore+created app.config for apikey

--- Kliens_App/Kliens_App/Form1.cs
+++ Kliens_App/Kliens_App/Form1.cs
@@ -1,3 +1,4 @@
+using System.Configuration;
 namespace Kliens_App
 {
     public partial class Form1 : Form
@@ -9,7 +10,8 @@ namespace Kliens_App
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            string apiKey = ConfigurationManager.AppSettings["apikulcs"];
+            MessageBox.Show(apiKey);
         }
     }
 }
